03d9d74367867ad4813b168485b8e8083b69394b
OgataiKhan <badellino.kevin@gmail.com>
2023-11-24 13:58:04 +0100

    feat: add message & image to dom with vue js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Scrooge McDuck is the richest duck in the world!',
+        scroogeImg: 'img/scrooge.jpg',
+        scroogeAlt: 'Scrooge McDuck'
       }
     }
   }).mount('#app')
